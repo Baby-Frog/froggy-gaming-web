2a93076ad2613a84539e0937727a5d52a5fcfa15
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-09-07 23:11:32 +0700

    :100:: Updated product list, you can now see products when you search

--- froggy-gaming/src/Products/productList/ProductList.js
+++ froggy-gaming/src/Products/productList/ProductList.js
@@ -1,11 +1,19 @@
 import React, { useEffect, useRef } from "react";
+import { useState } from "react";
+import useSWR from "swr";
+import { fetcher } from "../../config";
 import { useSearch } from "../../contexts/search-context";
 import useScrolled from "../../hooks/useScrolled";
 
 const ProductList = () => {
   const { query, setQuery } = useSearch();
-  console.log("log ~ ProductList ~ query", query);
+  const [nextPage, setNextPage] = useState(1);
   const { height, setIsScrolled } = useScrolled(300);
+  const { data } = useSWR(
+    `http://localhost:8386/api/v1/product/search/query=${query}`,
+    fetcher
+  );
+
   useEffect(() => {
     window.addEventListener("scroll", () => {
       if (window.innerWidth > 1024 && window.scrollY > 300) {
@@ -22,7 +30,26 @@ const ProductList = () => {
       isMounted.current = false;
     };
   }, []);
-  return <div></div>;
+  if (!data || !data.data) return;
+  const products = data?.data || [];
+  return (
+    <>
+      {products.length > 0 &&
+        products.map((item) => (
+          <ProductCard key={item.proId} item={item}></ProductCard>
+        ))}
+    </>
+  );
+};
+
+const ProductFilter = () => {};
+
+const ProductCard = ({ item }) => {
+  return (
+    <>
+      <span>{item.proName}</span>
+    </>
+  );
 };
 
 export default ProductList;
