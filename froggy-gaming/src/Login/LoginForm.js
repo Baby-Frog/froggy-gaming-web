de7a88c8b5045f5d24a53045b22cd16cad416163
TranDangKhoi <88824627+TranDangKhoi@users.noreply.github.com>
2022-09-14 00:14:49 +0700

    :+1: Change function's name

--- froggy-gaming/src/Login/LoginForm.js
+++ froggy-gaming/src/Login/LoginForm.js
@@ -3,13 +3,13 @@ import { useNavigate } from "react-router-dom";
 
 const LoginForm = () => {
   const navigate = useNavigate();
-  const doSomething = () => {
+  const handleSubmit = () => {
     // code trên này
     // navigate("/");
   };
   return (
     <div>
-      <form onSubmit={doSomething}>
+      <form onSubmit={handleSubmit}>
         <input type="text" id="username" name="username" />
         <label htmlFor="username">Username</label>
         <input type="text" id="password" name="password" />
